d068d7cf012f01a22131b2d85ad2ad6bf0ccb3df
CamiloEscudero <58115779+CamiloEscudero@users.noreply.github.com>
2020-12-15 10:39:30 -0500

    Primera Ejecucion

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import Principal from "./src/screens/stacknavigation";
+
+
+export default function App() {
+    return (
+        <Principal />
+    )
+};
